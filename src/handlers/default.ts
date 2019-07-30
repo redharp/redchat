2c2c1fe7980c4304bfb39cfed7e48581aca0825a
redharp <trevor.old@outlook.com>
2019-07-29 21:03:42 -0600

    added in memory user cache

--- src/handlers/default.ts
+++ src/handlers/default.ts
@@ -4,15 +4,19 @@ import { AWSURL } from "../config/constants";
 import { MessageSender } from "../sockets/send";
 
 const logger = new Logger().getLogger("defaultHandler");
+const currentSession: Set<string> = new Set();
 
 export const defaultHandler: APIGatewayProxyHandler = async (event, context) => {
     const { body } = event;
     const { connectionId, apiId, stage } = event.requestContext;
+    currentSession.add(connectionId as string);
 
     logger.info(`Incoming message from ${connectionId}: ${body}`);
     const endpoint: string = AWSURL(apiId, stage);
     const ms: MessageSender = new MessageSender(endpoint);
-    await ms.postMessage("Hello there!", connectionId as string);
+    for (const user of currentSession.values()) {
+        await ms.postMessage("Hello there!", user);
+    }
 
     return {
         body: "default",
